dfd2102d57722094b82beebcedeabded9b898961
Eduardo Nunez <eng91694@gmail.com>
2017-10-13 00:10:54 -0500

    Changed project description for todo list app

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@ var projects_json = [
 
 		"image":"/assets/projects/todo.png",
 
-		"info":"Built this web app for the purposes of learning React and handling user authentication with Google and Facebook OAuth services. This will help facilitate coding an upcoming project that requires user authentication.",
+		"info":"Built this web app for the purposes of learning React and handling user authentication with Google and Facebook OAuth services. Web app allows for creating todos under a user account, and list of todos are synched with a MongoDB database.",
 
 		"links":{
 			"main":"https://todos-reactjs.herokuapp.com",
