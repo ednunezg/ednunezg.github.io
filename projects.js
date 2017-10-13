8531922abae295f4e07700d15be32aab99d8cd00
Eduardo Nunez <eng91694@gmail.com>
2017-10-13 00:32:07 -0500

    Changed the description of todo project. Again

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@ var projects_json = [
 
 		"image":"/assets/projects/todo.png",
 
-		"info":"Built this for the purposes of learning React and handling user authentication with Google and Facebook OAuth services. This web app allows a logged in user to add todos to his profile. All todos are synched with a MongoDB database.",
+		"info":"Built this for the purposes of learning React and handling authentication with Google and Facebook OAuth services. This web app allows a logged in user to add todos to his profile. All todos are synched with a MongoDB database.",
 
 		"links":{
 			"main":"https://todos-reactjs.herokuapp.com",
