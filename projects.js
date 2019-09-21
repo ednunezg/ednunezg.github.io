8894c2de3d4ba71edcfd470a15464c0282e816cc
Ed Nunez <eng91694@gmail.com>
2019-09-21 11:25:21 -0700

    Update projects

--- projects.js
+++ projects.js
@@ -9,7 +9,8 @@ var projects_json = [
 		"info":"Create augmented reality enabled business cards, viewable on your smartphone without an app.",
 
 		"links":{
-			"main":"https://www.glimpsecard.com",
+			"moreinfo":"https://www.glimpsecard.com",
+                        "main":"https://app.glimpsecard.com"
 		},
 
 		"tags":["Node.js","React.js","JavaScript", "Shopify", "Stripe"]
